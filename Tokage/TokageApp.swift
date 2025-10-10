9fce269ae826332dd1be99db4caf5d90638da420
Mohit <mohit@hey.com>
2025-10-10 18:44:55 +0530

    Initial Commit

---
+++ Tokage/TokageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokageApp.swift
+//  Tokage
+//
+//  Created by Mohit on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TokageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TokageTests/TokageTests.swift
@@ -0,0 +1,17 @@
+//
+//  TokageTests.swift
+//  TokageTests
+//
+//  Created by Mohit on 10/10/25.
+//
+
+import Testing
+@testable import Tokage
+
+struct TokageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
